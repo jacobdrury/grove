ea5aebdd4330a21966a81b2ef894f443fd335ae5
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2025-06-06 00:21:18 -0500

    make command work no matter what directory you run it from (#3)

--- internal/util/fs.go
+++ internal/util/fs.go
@@ -11,12 +11,14 @@ func InDirectory(dir string, f func() error) error {
 		return err
 	}
 
+	slog.Debug("changing directory", slog.String("path", dir))
 	err = os.Chdir(dir)
 	if err != nil {
 		return err
 	}
 
 	defer func() {
+		slog.Debug("changing directory", slog.String("path", wd))
 		err = os.Chdir(wd)
 		if err != nil {
 			slog.Error("error restoring working directory", slog.String("workingDirectory", wd), slog.String("error", err.Error()))
